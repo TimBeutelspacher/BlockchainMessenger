850d579b308b833625fad88981a992cd9a580753
Tim Beutelspacher <46922380+TimBeutelspacher@users.noreply.github.com>
2020-05-21 17:12:17 +0200

    initial Commit

---
+++ Messenger.sol
@@ -0,0 +1,119 @@
+pragma solidity > 0.4.24;
+
+contract Messenger {
+    
+    uint chatCounter = 0;
+    mapping(uint => chat) public chats;  
+    
+    struct message {
+        string text;
+        address author;
+    }
+    
+    struct chat {
+        uint chatID;
+        uint messageCounter;
+        uint memberCounter;
+        mapping(uint => message) messages;
+        mapping(uint => address) members;
+    }
+    
+    function createChat() public {
+       
+        chat memory newChat = chat(chatCounter,0,1);
+        chats[chatCounter] = newChat;
+        chats[chatCounter].members[1] = msg.sender;
+        
+        chatCounter += 1;
+    }
+    
+    function createMessage(uint givenChatID, string memory givenText) public {
+        // Chat kann auch ohne Member existieren wenn man Chats verlassen kann
+        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
+        
+        message memory newMessage = message(givenText, msg.sender);
+        chats[givenChatID].messages[chats[givenChatID].messageCounter] = newMessage;
+        chats[givenChatID].messageCounter += 1;
+    }
+    
+    function getLatestMessage(uint givenChatID) view public returns(string memory) {
+        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(chats[givenChatID].messageCounter != 0, "There isn't a message in this chat!");
+        require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
+        
+        return chats[givenChatID].messages[chats[givenChatID].messageCounter - 1].text;
+    }
+    
+    function getAllMessages(uint givenChatID) view public returns(string memory) {
+        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(chats[givenChatID].messageCounter != 0, "There isn't a message in this chat!");
+        require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
+        
+        //string memory allMessages;
+        
+        string memory output;
+        string memory currentMessage;
+        string memory currentAuthor;
+        
+        for(uint i = 0; i < chats[givenChatID].messageCounter; i++){
+            /*string memory currentMessages = allMessages;
+            address authorsAddress = chats[givenChatID].messages[i].author;
+            allMessages = currentMessages + chats[givenChatID].members[authorsAddress] + chats[givenChatID].messages[i];
+            */
+            
+            currentMessage = chats[givenChatID].messages[i].text;
+            currentAuthor = addressToString(chats[givenChatID].messages[i].author);
+            output = string(abi.encodePacked(output, currentAuthor, ': ', currentMessage, '\n'));
+            
+        }
+        
+        return output;
+    }
+    
+    function isInChat(uint givenChatID, address givenAuthor) view internal returns(bool isMember) {
+        
+        uint memberIndex = 1;
+        
+        while(memberIndex <= chats[givenChatID].memberCounter) {
+            
+            if(chats[givenChatID].members[memberIndex] == givenAuthor){
+                return true;
+            }
+            
+            memberIndex += 1;
+        }
+        
+        return false;
+    }
+    
+    function addMember(uint givenChatID, address givenAddress) public {
+        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
+        require(isInChat(givenChatID, givenAddress) == false, "The given Address is already in this Chat!");
+        
+        chats[givenChatID].memberCounter += 1;
+        chats[givenChatID].members[chats[givenChatID].memberCounter] = givenAddress;
+    }
+    
+    function addressToString(address _addr) private pure returns(string memory) {
+        bytes32 value = bytes32(uint256(_addr));
+        bytes memory alphabet = "0123456789abcdef";
+    
+        bytes memory str = new bytes(42);
+        str[0] = '0';
+        str[1] = 'x';
+        for (uint i = 0; i < 20; i++) {
+            str[2+i*2] = alphabet[uint(uint8(value[i + 12] >> 4))];
+            str[3+i*2] = alphabet[uint(uint8(value[i + 12] & 0x0f))];
+        }
+        return string(str);
+    }
+    
+    function leaveChat(uint givenChatID) public {
+        require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
+        // Mappings können nicht gelöscht werden.
+        // chats[givenChatID].members[]
+    }
+    
+}
