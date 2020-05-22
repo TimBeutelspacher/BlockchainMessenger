0003122bfd37a648bcf7b5dc06868c81211ddf12
Tim Beutelspacher <46922380+TimBeutelspacher@users.noreply.github.com>
2020-05-22 12:20:35 +0200

    Update Messenger.sol

--- Messenger.sol
+++ Messenger.sol
@@ -4,6 +4,7 @@ contract Messenger {
     
     uint chatCounter = 0;
     mapping(uint => chat) public chats;  
+    mapping(address => string) public users;
     
     struct message {
         string text;
@@ -28,8 +29,7 @@ contract Messenger {
     }
     
     function createMessage(uint givenChatID, string memory givenText) public {
-        // Chat kann auch ohne Member existieren wenn man Chats verlassen kann
-        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(givenChatID < chatCounter, "The given ChatID doens't exist yet!");
         require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
         
         message memory newMessage = message(givenText, msg.sender);
@@ -37,16 +37,16 @@ contract Messenger {
         chats[givenChatID].messageCounter += 1;
     }
     
-    function getLatestMessage(uint givenChatID) view public returns(string memory) {
-        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+    /*function getLatestMessage(uint givenChatID) view public returns(string memory) {
+        require(givenChatID < chatCounter, "The given ChatID doens't exist yet!");
         require(chats[givenChatID].messageCounter != 0, "There isn't a message in this chat!");
         require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
         
         return chats[givenChatID].messages[chats[givenChatID].messageCounter - 1].text;
-    }
+    }*/
     
     function getAllMessages(uint givenChatID) view public returns(string memory) {
-        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(givenChatID < chatCounter, "The given ChatID doens't exist yet!");
         require(chats[givenChatID].messageCounter != 0, "There isn't a message in this chat!");
         require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
         
@@ -58,7 +58,8 @@ contract Messenger {
         for(uint i = 0; i < chats[givenChatID].messageCounter; i++){
             
             currentMessage = chats[givenChatID].messages[i].text;
-            currentAuthor = addressToString(chats[givenChatID].messages[i].author);
+            //currentAuthor = addressToString(chats[givenChatID].messages[i].author);
+            currentAuthor = users[chats[givenChatID].messages[i].author];
             output = string(abi.encodePacked(output, currentAuthor, ': ', currentMessage, '\n'));
             
         }
@@ -83,7 +84,7 @@ contract Messenger {
     }
     
     function addMember(uint givenChatID, address givenAddress) public {
-        require(chats[givenChatID].memberCounter != 0, "The given ChatID doens't exist yet!");
+        require(givenChatID < chatCounter, "The given ChatID doens't exist yet!");
         require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
         require(isInChat(givenChatID, givenAddress) == false, "The given Address is already in this Chat!");
         
@@ -107,8 +108,26 @@ contract Messenger {
     
     function leaveChat(uint givenChatID) public {
         require(isInChat(givenChatID, msg.sender), "You aren't a member of this chat!");
-        // Mappings können nicht gelöscht werden.
-        // chats[givenChatID].members[]
+         
+         uint memberIndex = 1;
+        
+        while(memberIndex <= chats[givenChatID].memberCounter) {
+            
+            if(chats[givenChatID].members[memberIndex] == msg.sender){
+                chats[givenChatID].members[memberIndex] = address(0);
+                break;
+            }
+            
+            memberIndex += 1;
+        }
+        
+        chats[givenChatID].memberCounter -= 1;
+    }
+    
+    function setNickname(string memory givenNickname) public {
+        
+        users[msg.sender] = givenNickname;
+        
     }
     
 }
