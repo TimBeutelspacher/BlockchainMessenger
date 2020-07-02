2791a0f9f3e4ec89f9e5f67b44d336b109a734ba
FelixRapp <48477182+FelixRapp@users.noreply.github.com>
2020-07-02 18:42:03 +0200

    Update Chat.sol

--- src/Chat.sol
+++ src/Chat.sol
@@ -1,42 +1,49 @@
 pragma solidity ^0.6.6;
 
-import "./Message.sol";
 import "./Messenger.sol";
+import "./Message.sol";
+import "./AllUsers.sol";
 
 contract Chat {
     
     //Variablen
     Messenger private messenger; 
-    uint private messageCounter = 0;
-    mapping (uint => Message) private messages;
+    AllUsers private users;
+    uint private chatID;
+    address private latestMessage;
     uint private memberCounter = 0;
     mapping (uint => address) private members;
     
-    constructor(Messenger _messenger)public{
+    constructor(Messenger _messenger, AllUsers _users, uint _chatID)public{
         messenger = _messenger;
+        users = _users;
+        chatID = _chatID;
     }
     
     //Funktion um diesem Chat beizutreten
     function joinChat() public{
         require(memberCounter == 0 || isInChat(msg.sender) == false, "You are already in this Chat!");
-        members[memberCounter] = msg.sender;
-        memberCounter++;
+        members[memberCounter++] = msg.sender;
+        if (chatID == 0){
+           users.setJoinChatCertified(msg.sender);
+        }        
     }
     
     //Funktion um eine Nachricht in den Chat zu schreiben
     function createMessage(string memory _message) public{
         require (isInChat(msg.sender), "You aren't a member of this chat!");
-        address previousMessage = address(messages[messageCounter - 1]);
-        Message message = new Message(messenger, _message, msg.sender, previousMessage);
-        messages[messageCounter] = message;
-        messageCounter++;
+        Message message = new Message(messenger, users, _message, msg.sender, latestMessage);
+        latestMessage = address(message);
+        
+        if(keccak256(abi.encodePacked(_message)) == keccak256(abi.encodePacked("HdM")) && chatID == 0) {
+            users.setMessageCertified(msg.sender);
+        }
     }
     
     //Funktion um sich die letzte Nachricht eines Chats ausgeben zu lassen.
-    function getLatestMessage() public view returns(Message){
+    function getLatestMessage() public view returns(address){
         require (isInChat(msg.sender), "You aren't a member of this chat!");
-        require (messageCounter > 0, "There isn't a message in this chat!");
-        return messages[messageCounter - 1];
+        return latestMessage;
     }
     
     //Überprüfung ob eine Adresse Mitglied eines Chats ist.
