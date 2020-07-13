33871a879e5490da8903c7669b22d6cda764f94e
FelixRapp <48477182+FelixRapp@users.noreply.github.com>
2020-07-13 17:35:40 +0200

    Update Message.sol

--- src/Message.sol
+++ src/Message.sol
@@ -1,21 +1,14 @@
 pragma solidity ^0.6.6;
 
-import "./Messenger.sol";
-import "./AllUsers.sol";
-
 contract Message {
     
     //Variablen
-    Messenger private messenger;
-    AllUsers private users;
     string public text;
-    address private author;
-    address private previousMessage;
+    address public author;
+    address public previousMessage;
     
     //Konstruktor
-    constructor(Messenger _messenger, AllUsers _users, string memory _text, address _author, address _previousMessage) public{
-        messenger = _messenger;
-        users = _users;
+    constructor(string memory _text, address _author, address _previousMessage) public{
         text = _text;
         author = _author;
         previousMessage = _previousMessage;
@@ -23,33 +16,7 @@ contract Message {
     
     //Funktion um die vorherige Nachricht in diesem Chat sich geben zu lassen.
     function getPreviousMessage() public view returns(address){
-        //require(previousMessage  != 0x0000000000000000000000000000000000000000, "This is the first message of this chat!");
+        require(previousMessage  != 0x0000000000000000000000000000000000000000, "This is the first message of this chat!");
         return previousMessage;
     }
-    
-    //Funktion, welche die Adresse des Autors und desen Nicknamen (falls vorhanden) ausgibt.
-    function getAuthor() public view returns(string memory){
-        string memory output;
-        string memory nickname = users.getNickname(author);
-        if (keccak256(abi.encodePacked((nickname))) == keccak256(abi.encodePacked(("")))){
-            nickname = "NoName";
-        }
-        output = string(abi.encodePacked(addressToString(author), " (", nickname, ")"));
-        return output;
-    }
-    
-    // Funktion um aus einer Adresse einen String zu produzieren
-    function addressToString(address _addr) private pure returns(string memory) {
-        bytes32 value = bytes32(uint256(_addr));
-        bytes memory alphabet = "0123456789abcdef";
-    
-        bytes memory str = new bytes(42);
-        str[0] = '0';
-        str[1] = 'x';
-        for (uint i = 0; i < 20; i++) {
-            str[2+i*2] = alphabet[uint(uint8(value[i + 12] >> 4))];
-            str[3+i*2] = alphabet[uint(uint8(value[i + 12] & 0x0f))];
-        }
-        return string(str);
-    } 
 }
